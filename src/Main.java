bb9ccc238c6347816c9352774af08bbbea80bb5b
haripriya-0712 <haripriyasudhakar712@gmail.com>
2026-04-09 10:06:19 +0530

    Featur9/UC9

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,47 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Bogie {
+    String name;
+    int capacity;
+    String type;
+
+    Bogie(String name, int capacity, String type) {
+        this.name = name;
+        this.capacity = capacity;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + capacity + ")";
+    }
+}
 
 public class TrainConsistApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Train Consist Management App");
-        Set<String> bogieIDs = new HashSet<>();
-        bogieIDs.add("BG101");
-        bogieIDs.add("BG102");
-        bogieIDs.add("BG103");
-        bogieIDs.add("BG101");
-        bogieIDs.add("BG102");
-        System.out.println("Unique Bogie IDs in Train:");
-        System.out.println(bogieIDs);
+        System.out.println("=== Train Consist Management App ===");
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72, "Passenger"));
+        bogies.add(new Bogie("AC Chair", 56, "Passenger"));
+        bogies.add(new Bogie("First Class", 40, "Passenger"));
+        bogies.add(new Bogie("Sleeper", 72, "Passenger"));
+        bogies.add(new Bogie("Rectangular", 100, "Goods"));
+        bogies.add(new Bogie("Cylindrical", 120, "Goods"));
+        Map<String, List<Bogie>> grouped =
+                bogies.stream()
+                        .collect(Collectors.groupingBy(b -> b.type));
+        System.out.println("\nGrouped Bogies:");
+
+        for (String key : grouped.keySet()) {
+            System.out.println("\nType: " + key);
+            for (Bogie b : grouped.get(key)) {
+                System.out.println("  " + b);
+            }
+        }
 
         System.out.println("\nProgram continues...");
     }
