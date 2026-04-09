2903a715673239e31d0c3f1ab9d67a7a32b28ee3
haripriya-0712 <haripriyasudhakar712@gmail.com>
2026-04-09 09:29:32 +0530

    Feature3/UC3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.HashSet;
+import java.util.Set;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Train Consist Management App");
+        Set<String> bogieIDs = new HashSet<>();
+        bogieIDs.add("BG101");
+        bogieIDs.add("BG102");
+        bogieIDs.add("BG103");
+        bogieIDs.add("BG101");
+        bogieIDs.add("BG102");
+        System.out.println("Unique Bogie IDs in Train:");
+        System.out.println(bogieIDs);
+
+        System.out.println("\nProgram continues...");
     }
 }
